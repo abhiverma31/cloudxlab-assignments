006a36ce19439b5545eeec899423918767a0b3d6
Abhishek Verma <abhishekverma@192.168.1.4>
2026-04-25 18:45:31 +0530

    gradient descent

--- minimum_value_of_expression_gradient_descent_approach.py
+++ minimum_value_of_expression_gradient_descent_approach.py
@@ -3,7 +3,10 @@ def f(a):
     #return a ** 4 - 3 * a ** 3 - 5 * a + 10
     return 13 * a ** 2 - 10 * a + 50
 
-def find_min(f):  
+def E_func(a,b): # a, b are model parameters
+    return (2*a + b - 10) ** 2 + (3*a + b -30) ** 2
+
+def find_min_one_model_parameter(f):  
     eps = .000001
     a = 10
     #slope = (f(a + eps) - f(a)) / eps
@@ -14,7 +17,7 @@ def find_min(f):
         #print(f"diff is {slope}")
         
         if slope > -.0001 and slope < .0001:
-            print(f'returing with slope as => {slope} and a as => {a}')
+            #print(f'returing with slope as => {slope} and a as => {a}')
             return a, f"{slope:.10f}"
         
         a = a - eps * slope
@@ -37,7 +40,31 @@ def find_min(f):
         #     diff = (f(a + eps) - f(a)) / eps
         #     print(f'diff2 -> {diff}') 
         
+        
+def find_min_two_model_parameters(f):    
+    eps = .000001
+    movement_measure = .001
+    a = 0
+    b = 0
+    
+    while True :
+        diff_a = (f(a+eps, b) - f(a,b)) / eps
+        diff_b = (f(a, b+eps) - f(a,b)) / eps
+        
+        if diff_a > -.0001 and diff_a < .0001 and  diff_b > -.0001 and diff_b < .0001 :            
+            return a, b, f"{diff_a:.10f}", f"{diff_b:.10f}"
+        
+        a = a - movement_measure * diff_a
+        b = b - movement_measure * diff_b
+        
 if __name__ == '__main__':
-    a, slope = find_min(f)
+    print('1 model parameter')
+    a, slope = find_min_one_model_parameter(f)
     print(f'The minimum value of function is {f(a)} and it occurs at point a = {a}')
+    
+    print('-' * 80)
+    
+    print('2 model parameters:')
+    a1, b1, slope_a, slope_b = find_min_two_model_parameters(E_func)
+    print(f'The minimum value of function is {E_func(a1,b1)} and it occurs at point a = {a1}, b = {b1}')
     
